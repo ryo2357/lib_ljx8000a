01707797ef5bc7dffa66692ef459194206e4c406
ryo2357 <miyawakir@hosokawa-yoko.co.jp>
2023-08-09 16:56:42 +0900

    build.rsの更新

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
-use std::env;
+// use std::env;
 
 fn main() {
-    let mut here = env::current_dir().unwrap();
-    here.push("vendor");
+    // let mut here = env::current_dir().unwrap();
+    // here.push("vendor");
     // 親ディレクトリは探しにいけなかった
     // println!(
     //     "cargo:rustc-link-search=native={}",
     //     format!("{}", here.display())
     // );
-    println!("cargo:rustc-link-search=native={}", here.display());
+    // println!("cargo:rustc-link-search=native={}", here.display());
+    println!("cargo:rustc-link-search=native=./vendor/");
     cc::Build::new().file("c_src/bridge.c").compile("bridge");
 }
